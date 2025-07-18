4feb498f8b4dce31a4a8e327b3387f72ba45991a
Alexander Sporn <github@alexsporn.de>
2025-07-18 12:14:15 +0200

    fix: revert build.rs to vanilla one from app-boilerplate-rust

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -1,8 +1,3 @@
-use std::env;
-
 fn main() {
     println!("cargo:rerun-if-changed=script.ld");
-    if let Ok(path) = env::var("NEWLIB_LIB_PATH") {
-        println!("cargo:rustc-link-search={path}");
-    }
 }
